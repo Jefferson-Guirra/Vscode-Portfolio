632a0a32a4411a061a2bf319c8788570cd1537dc
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-12 23:51:21 -0300

    feat: create first home page style

--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.section`
-  background-color: red;
+  background-color: #222222;
+  width: 100%;
+  height: 100vh;
+  color: #d1e7e7;
+
+  header {
+    text-align: center;
+  }
 `
