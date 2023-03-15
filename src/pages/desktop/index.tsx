b487ae65a2a61648deeb638ac5cebb883a74da3a
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-14 22:02:35 -0300

    refactor: remove header tag

--- src/pages/desktop/index.tsx
+++ src/pages/desktop/index.tsx
@@ -1,39 +1,28 @@
 import React from 'react'
 import styles from '../../styles/desktop.module.css'
-import Clock from '../../components/header/clock/Clock'
-import { ClockProps } from '../../protocols/clock'
 import Image from 'next/image'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const index = () => {
-  const clockProps: ClockProps = {
-    language: 'pt-Br',
-    dateTimeFormatOptions: {
-      day: '2-digit',
-      month: 'short',
-      hour: '2-digit',
-      minute: '2-digit',
-    },
-  }
   return (
-    <section className={styles.container}>
-      <header className={styles.header}>
-        <Clock
-          language={clockProps.language}
-          dateTimeFormatOptions={clockProps.dateTimeFormatOptions}
-        />
-      </header>
-      <nav className={styles.nav}>
-        <Link href="/">
-          <Image
-            src="/images/vscode.svg"
-            alt=" vscode icon"
-            width={24}
-            height={24}
-          />{' '}
-        </Link>
-      </nav>
-    </section>
+    <>
+      <Head>
+        <title> Desktop </title>
+      </Head>
+      <section className={styles.container}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <Image
+              src="/images/vscode.svg"
+              alt=" vscode icon"
+              width={24}
+              height={24}
+            />{' '}
+          </Link>
+        </nav>
+      </section>
+    </>
   )
 }
 
