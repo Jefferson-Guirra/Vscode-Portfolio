8a6f7e20ea36deb640acae2d2f34051ffee3e954
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-14 21:51:00 -0300

    refactor: remove header from component UserPage

--- src/components/forms/form/userPageForm.tsx
+++ src/components/forms/form/userPageForm.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Clock from '../../header/clock/Clock'
-import { ClockProps } from '@/protocols/clock'
 import { BiUser } from 'react-icons/bi'
 import Form from './index'
 import { FormProps } from './form-protocols'
@@ -9,24 +7,8 @@ import * as C from './user-form'
 type Props = FormProps
 
 const UserPage = ({ button, handleSubmit, inputs, linkProps }: Props) => {
-  const clockProps: ClockProps = {
-    language: 'pt-BR',
-    dateTimeFormatOptions: {
-      day: '2-digit',
-      month: 'short',
-      hour: '2-digit',
-      minute: '2-digit',
-    },
-  }
-
   return (
     <C.Container>
-      <header>
-        <Clock
-          language={clockProps.language}
-          dateTimeFormatOptions={clockProps.dateTimeFormatOptions}
-        />
-      </header>
       <article className="content-box">
         <div className="content">
           <span className="user-icon">
