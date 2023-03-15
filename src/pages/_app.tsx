263a9265f6d2c5a1a2e467cf54773e00d9adb9d8
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-14 22:00:48 -0300

    feat: making the HeaderContainer  a global component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import HeaderContainer from '../components/header/header-container'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeaderContainer />
+      <Component {...pageProps} />
+    </>
+  )
 }
