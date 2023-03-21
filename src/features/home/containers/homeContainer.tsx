4b808ef3d5d6f244c6e389ed6616b8e874ec363f
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-21 20:36:28 -0300

    feat: create homeContainer Component

---
+++ src/features/home/containers/homeContainer.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import React from 'react'
+import HomePage from '../pages/Homepage'
+import styles from './homeContainer.module.css'
+import { Props } from '../constants/props'
+
+export default function HomeContainer({ description }: Props) {
+  return (
+    <>
+      <Head>
+        <title>login</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.container}>
+        <HomePage />
+      </main>
+    </>
+  )
+}
