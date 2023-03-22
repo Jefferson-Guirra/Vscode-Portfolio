fb964320a3488afbefd251da73624e39cdb98b0b
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-22 15:29:29 -0300

    refactor: add DesktopPage component

--- src/pages/desktop/index.tsx
+++ src/pages/desktop/index.tsx
@@ -1,29 +1,7 @@
-import React from 'react'
-import styles from '../../styles/desktop.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-import Head from 'next/head'
+import DesktopContainer from '@/features/desktop/containers/DesktopContainer'
 
 const index = () => {
-  return (
-    <>
-      <Head>
-        <title> Desktop </title>
-      </Head>
-      <section className={styles.container}>
-        <nav className={styles.nav}>
-          <Link href="/">
-            <Image
-              src="/images/vscode.svg"
-              alt=" vscode icon"
-              width={24}
-              height={24}
-            />{' '}
-          </Link>
-        </nav>
-      </section>
-    </>
-  )
+  return <DesktopContainer />
 }
 
 export default index
