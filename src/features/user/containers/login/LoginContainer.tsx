d938a2c7b6177832fa1f8582acf65bd9fa4086dc
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-22 00:55:34 -0300

    feat: create LoginContainer component

---
+++ src/features/user/containers/login/LoginContainer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from 'next/head'
+import GlobalUserContainer from '../../../../containers/user/GlobalUserContainer'
+import Login from '../../pages/Login'
+
+const LoginContainer = () => {
+  return (
+    <>
+      <Head>
+        <title>login</title>
+        <meta name="description" content="Login user page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalUserContainer>{Login()}</GlobalUserContainer>
+    </>
+  )
+}
+
+export default LoginContainer
