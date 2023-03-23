306e3e15f7ebf631d2318af01305abf006c99d85
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-23 20:50:57 -0300

    feat: getting the necessary cookies for the vscodeContainer component

--- src/pages/desktop/index.tsx
+++ src/pages/desktop/index.tsx
@@ -1,7 +1,21 @@
 import DesktopContainer from '@/features/desktop/containers/DesktopContainer'
+import { GetServerSideProps } from 'next'
+import nookies from 'nookies'
 
-const index = () => {
-  return <DesktopContainer />
+interface Props {
+  vscodeIsOpen: string
 }
-
+const index = ({ vscodeIsOpen }: Props) => {
+  return <DesktopContainer vscodeIsOpen={vscodeIsOpen} />
+}
 export default index
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const vscodeIsOpen =
+    nookies.get(ctx).vscodeIsOpen === 'true' ? 'true' : 'false'
+  return {
+    props: {
+      vscodeIsOpen,
+    },
+  }
+}
