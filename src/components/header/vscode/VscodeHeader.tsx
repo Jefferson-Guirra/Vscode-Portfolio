da276c53bbb7f901565e129f52c42bb2f8962f5c
jefferson guirra <jeffersonguirra8@gmail.com>
2023-03-23 21:03:50 -0300

    feat: creating function to handle vscode closing

--- src/components/header/vscode/VscodeHeader.tsx
+++ src/components/header/vscode/VscodeHeader.tsx
@@ -2,8 +2,12 @@ import styles from './styles.module.css'
 import { MdMinimize, MdClose } from 'react-icons/md'
 import { BiExpandAlt } from 'react-icons/bi'
 import Link from 'next/link'
+import { setCookie } from 'nookies'
 
 export const VscodeHeader = () => {
+  const handleCloseVscode = () => {
+    setCookie(null, 'vscodeIsOpen', 'false')
+  }
   return (
     <header className={styles.header}>
       <article className={styles.actions}>
@@ -13,7 +17,11 @@ export const VscodeHeader = () => {
         <Link href="/" className={styles.icons}>
           <BiExpandAlt size={15} color="#000000" />
         </Link>
-        <Link href="/desktop" className={styles.icons}>
+        <Link
+          onClick={handleCloseVscode}
+          href="/desktop"
+          className={styles.icons}
+        >
           <MdClose size={15} color="#000000" />
         </Link>
       </article>
