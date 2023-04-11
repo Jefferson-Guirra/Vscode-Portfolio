3dc1c887d59888c23c9deb63934c2b6fddcfaab1
jefferson guirra <jeffersonguirra8@gmail.com>
2023-04-10 22:53:22 -0300

    feat: add open and closed file checker function in order to change open file state

--- src/layouts/layout-components/header/vscode/FilersOpen.tsx
+++ src/layouts/layout-components/header/vscode/FilersOpen.tsx
@@ -1,15 +1,24 @@
 import { useVscodeContext } from '@/context/vscode/vscode'
 import { VscClose } from 'react-icons/vsc'
+import { File } from '@/@types'
 import * as C from './filersOpen'
 
 export const FilersOpen = () => {
   const { filersOpen, filerOpen, updateFilersOpen } = useVscodeContext()
+
+  const handleFile = (file: File) => {
+    if (filerOpen === file) {
+      updateFilersOpen.updateFileOpen({} as File)
+      updateFilersOpen.close(file)
+    }
+    updateFilersOpen.close(file)
+  }
   return (
     <C.container>
       {filersOpen.map((file) => (
         <C.file isOpen={filerOpen.index === file.index} key={file.index}>
           <p>{file.value}</p>
-          <span onClick={() => updateFilersOpen.close(file)}>
+          <span onClick={() => handleFile(file)}>
             <VscClose size={20} color="#646464" />
           </span>
         </C.file>
