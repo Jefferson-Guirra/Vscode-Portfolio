4d0b21cc397d36c1c2f87cca3e9950200e70defb
jefferson guirra <jeffersonguirra8@gmail.com>
2023-04-19 22:43:58 -0300

    feat: implement UpdateFoldersOpen in factory method

--- src/context/factories/handle-cookie.ts
+++ src/context/factories/handle-cookie.ts
@@ -1,20 +1,24 @@
 import { SetCookie } from '../cookies/insert-cookie'
 import { UpdateFilers } from '../cookies/update-filers-open-cookie'
 import { UpdateFile } from '../cookies/update-file-open'
+import { UpdateFolders } from '../cookies/update-folders-open-cookie'
 
 interface HandleCookie {
   insertCookie: SetCookie
   updateFile: UpdateFile
   updateFilers: UpdateFilers
+  updateFolders: UpdateFolders
 }
 
 export const handleCookie = (): HandleCookie => {
   const insertCookie = new SetCookie()
   const updateFile = new UpdateFile(insertCookie)
   const updateFilers = new UpdateFilers(insertCookie)
+  const updateFolders = new UpdateFolders(insertCookie)
   return {
     insertCookie,
     updateFile,
     updateFilers,
+    updateFolders,
   }
 }
