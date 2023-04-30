91541e07d1fdb93cc05bc9a4d5cd86f12520e0fd
jefferson guirra <jeffersonguirra8@gmail.com>
2023-04-29 22:55:28 -0300

    feat: create data used in FrontEndContainer and BackEndContainer components

---
+++ src/components/layouts/constants/data-skills-page.ts
@@ -0,0 +1,29 @@
+export const dataTechs: any = {
+  Css: 'Css é uma liguagem de folhas de estilos usada para estilizar elementos escritos numa liguagem de marcação como HTML.',
+  Saas: 'Saas é um pré-processador css usado para adicionar novos recursos ao css, tornando o ambiente de uso mais organizado e intuitivo.',
+  'Styled-components':
+    'Styled-components é um biblioteca que nos permite usar o conceito CSS-IN-JS, ou seja, nos permite escrever códigos CSS dentro do Javascript',
+  Bootstrap:
+    'Bootstrap é um framework usado para potencializar o uso do css, facilitando a estilização dos elementos.',
+  JavaScript:
+    'JavaScript é uma linguagem de programação usada para criar interatividade e dinamismo em páginas web, podendo ser executado no lado do cliente ou servidor',
+  Typescript:
+    'TypeScript é uma extensão da linguagem JavaScript que adiciona recursos como tipagem estática, interfaces e outros recursos orientados a objetos, ajudando a escrever códigos mais seguros e escaláveis',
+  React:
+    'React é uma biblioteca Javascript focada na criação de interfaces de usuário de maneira componentizada.',
+  Redux:
+    'Redux é uma biblioteca JavaScript que gerencia o estado de aplicativos web, com ele  é possível atualizar o estado do aplicativo de forma previsível e segura',
+  Next: 'Next é uma estrutura web que permite funcionalidades como renderização ao lado do servidor e geração de sites estaticos para web baseado em React',
+  CMS: 'Cms é um Sistema online usado para criar, editar, publicar e gerenciar conteúdos de paginas web.',
+  Prismic:
+    'Prismic é um CMS usado para criação e gerenciamento de conteúdo com recursos avançados de personalização, colaboração, integração e SEO.',
+  Nodejs:
+    'Node.js é um ambiente de tempo de execução JavaScript no lado do servidor, rápido e escalável usado para criar aplicativos de rede, como servidores web e APIs',
+  Tools: 'Ferramentas usadas para testes do servidor',
+  Postman:
+    'O Postman é uma ferramenta que facilita o processo de criação, teste e documentação de APIs. ',
+  Insomnia:
+    'Insomnia é uma ferramenta de teste de API e gerenciamento de solicitações HTTP.',
+  'Autenticação-JWT':
+    'JWT (JSON Web Token) é um padrão para criação de tokens de autenticação em aplicativos web. A autenticação JWT é baseada em token e é usada para autenticar e autorizar usuários. ',
+}
