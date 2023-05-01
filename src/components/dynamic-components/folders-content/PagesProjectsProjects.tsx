8a24b70d02e9b5c4a718f45dd77a8c66f4e0a1a2
jefferson guirra <jeffersonguirra8@gmail.com>
2023-04-30 22:54:05 -0300

    feat: add ProjectCard component

--- src/components/dynamic-components/folders-content/PagesProjectsProjects.tsx
+++ src/components/dynamic-components/folders-content/PagesProjectsProjects.tsx
@@ -1,5 +1,17 @@
+import styles from './projects.module.css'
+import { ProjectCard } from '@/components'
+import { projectData } from '../../constants/project-data'
+
 const PagesProjectsProjects = () => {
-  return <div>PagesProjectsProjects</div>
+  return (
+    <main className={styles.container}>
+      <section className={styles.content}>
+        {projectData.map((props) => (
+          <ProjectCard key={props.title} {...props} />
+        ))}
+      </section>
+    </main>
+  )
 }
 
 export default PagesProjectsProjects
