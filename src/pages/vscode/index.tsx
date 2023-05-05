ac96c1e5758c174416f79b5ff5c8d48dcffc4f6f
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-05 17:04:52 -0300

    feat: add page title

--- src/pages/vscode/index.tsx
+++ src/pages/vscode/index.tsx
@@ -1,10 +1,18 @@
 import VscodeContainer from '@/features/vscode/containers/VscodeContainer'
 import MyVscodeLayout from '@/layouts/vscode/MyVscodeLayout'
 import PageWithLayoutType from '@/layouts/@types'
+import Head from 'next/head'
 import { FC } from 'react'
 
 const index: FC = () => {
-  return <VscodeContainer />
+  return (
+    <>
+      <Head>
+        <title>Vscode</title>
+      </Head>
+      <VscodeContainer />
+    </>
+  )
 }
 ;(index as PageWithLayoutType).layout = MyVscodeLayout
 export default index
