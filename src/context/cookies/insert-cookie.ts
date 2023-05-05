0d287108f2128d8b0f8a4f5d83adc93db221e8d3
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-05 19:52:27 -0300

    feat: make cookies available daily

--- src/context/cookies/insert-cookie.ts
+++ src/context/cookies/insert-cookie.ts
@@ -2,6 +2,9 @@ import { CookieProps, InsertCookie } from '../@types/insert-cookie'
 import { setCookie } from 'nookies'
 export class SetCookie implements InsertCookie {
   insert(cookie: CookieProps) {
-    setCookie(null, cookie.name, JSON.stringify(cookie.value))
+    setCookie(null, cookie.name, JSON.stringify(cookie.value), {
+      maxAge: 86400,
+      path: '/',
+    })
   }
 }
