9c89fa929fb6551d362539187c4c6b63db817252
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-06 14:37:25 -0300

    fix: ensure component responsiveness

--- src/layouts/vscode/styles.ts
+++ src/layouts/vscode/styles.ts
@@ -4,13 +4,10 @@ export const container = styled.main`
   background-color: #1e1e1e;
   display: flex;
   flex: 1;
-  overflow: hidden;
   flex-direction: column;
   .content-box {
     position: relative;
-    height: calc(100% - 23px);
     display: flex;
-    overflow: hidden;
   }
   .content-box .nav {
     display: flex;
@@ -18,7 +15,6 @@ export const container = styled.main`
 
   .content {
     position: relative;
-    overflow: hidden;
     flex: 1;
     display: flex;
     flex-direction: column;
