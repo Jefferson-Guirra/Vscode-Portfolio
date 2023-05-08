993a29492d710586f2624cfe29b132038864ccf6
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-08 16:10:57 -0300

    feat: create handleResize function to handle Rnd element dimensions

--- src/components/layouts/draggable-resizable-element/vscode/VscodeBox.tsx
+++ src/components/layouts/draggable-resizable-element/vscode/VscodeBox.tsx
@@ -8,20 +8,38 @@ import {
 import { useVscodeContext } from '@/context/vscode/vscode'
 import Vscode from '../../../../features/vscode/pages/Vscode'
 import { Rnd } from 'react-rnd'
+import { useEffect, useState } from 'react'
+import { debounce } from '@/utils/debounce'
 
 export const VscodeBox = () => {
+  const [width, setWidth] = useState('80%')
+  const [height, setHeight] = useState('50%')
   const { viewFilers } = useVscodeContext()
+
+  const handleResize = () => {
+    setHeight(window.innerHeight * 0.5 + 'px')
+    setWidth(window.innerWidth * 0.8 + 'px')
+  }
+
+  const debounceResizeWindow = debounce(handleResize, 200)
+
+  useEffect(() => {
+    window.addEventListener('resize', debounceResizeWindow)
+    return () => {
+      window.removeEventListener('resize', debounceResizeWindow)
+    }
+  }, [])
   return (
     <Rnd
       default={{
         x: 50,
-        y: 300,
-        width: '80%',
-        height: '50%',
+        y: 50,
+        width,
+        height,
       }}
       maxWidth={'100%'}
-      minWidth={'30%'}
-      minHeight={400}
+      minWidth={'40%'}
+      minHeight={'40%'}
       bounds="window"
       cancel="cancel"
       className="modal"
