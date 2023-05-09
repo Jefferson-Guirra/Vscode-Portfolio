5ef96a90828ee89e6cbaafba21c10fbf47fb1284
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-09 16:10:22 -0300

    feat: create project icon data

---
+++ src/constants/icon-project-data.ts
@@ -0,0 +1,44 @@
+export const iconProjectData = [
+  { name: 'Movie Net', href: 'https://movies-net.netlify.app/', icon: 'movie' },
+  { name: 'Literando', href: 'https://literando.vercel.app/', icon: 'book' },
+  {
+    name: 'Fundação Dev',
+    href: 'https://fundacao-dev.vercel.app/',
+    icon: 'foundation',
+  },
+  {
+    name: 'Todo List',
+    href: 'https://projeto-06-lista-de-tarefas.netlify.app/',
+    icon: 'list',
+  },
+  {
+    name: 'Vscode Portfolio',
+    href: 'https://vscode-portfolio-developer.vercel.app/',
+    icon: 'visual-studio-code',
+  },
+  {
+    name: 'Meu Board',
+    href: 'https://meu-board-three.vercel.app/',
+    icon: 'board-list',
+  },
+  {
+    name: 'Portfolio',
+    href: 'https://my-storage-jefferson.netlify.app/',
+    icon: 'space',
+  },
+  {
+    name: 'Doctor Care',
+    href: 'https://doctor-care-clinica.netlify.app/',
+    icon: 'doctor',
+  },
+  {
+    name: 'Animais fantásticos',
+    href: 'https://jefferson-guirra.github.io/-Projetos/projeto-02/',
+    icon: 'frog',
+  },
+  {
+    name: 'Caravan',
+    href: 'https://caravan-viagens-empresa.netlify.app/',
+    icon: 'bus',
+  },
+]
