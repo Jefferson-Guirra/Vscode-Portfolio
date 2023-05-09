0d966c8147c81cb59dce8ca7737b95c69968a005
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-08 21:42:14 -0300

    refactor: remove unused element

--- src/features/desktop/pages/Desktop.tsx
+++ src/features/desktop/pages/Desktop.tsx
@@ -4,54 +4,37 @@ import * as C from './styles'
 import { projectData } from '@/components/constants/project-data'
 import { DragElement, VscodeBox } from '@/components'
 import { useVscodeContext } from '@/context/vscode/vscode'
-import { parseCookies } from 'nookies'
 
 const Desktop = () => {
-  const { vscodeIsMinimize, insertCookie, handleVscodeMinimize } =
-    useVscodeContext()
-  const vscodeOpenCookie = parseCookies().vscodeIsOpen
-    ? JSON.parse(parseCookies().vscodeIsOpen)
-    : false
+  const { vscodeIsMinimize } = useVscodeContext()
 
-  const handleOpenVscode = () => {
-    handleVscodeMinimize.update(false)
-    insertCookie.insert({ name: 'vscodeIsOpen', value: true })
-  }
   return (
-    <C.container>
-      <section className="content">
-        {projectData.map((project) => (
-          <DragElement key={project.title} width={180} height={68}>
-            <Link
-              target="_blank"
-              href={project.links[1].href}
-              className="project"
-              key={project.title}
-            >
-              <Image
-                src={`/images/desktop/${project.iconName}.svg`}
-                alt={`${project.iconName} icon`}
-                width={50}
-                height={50}
-                priority
-              />
-              <p key={project.title}>{project.title}</p>
-            </Link>
-          </DragElement>
-        ))}
-      </section>
-      {vscodeIsMinimize && <VscodeBox />}
-      <C.nav vscodeIsOpen={vscodeOpenCookie}>
-        <Link href="/vscode" onClick={handleOpenVscode}>
-          <Image
-            src="/images/vscode.svg"
-            alt=" vscode icon"
-            width={32}
-            height={32}
-          />
-        </Link>
-      </C.nav>
-    </C.container>
+    <>
+      <C.container>
+        <section className="content">
+          {projectData.map((project) => (
+            <DragElement key={project.title} width={180} height={68}>
+              <Link
+                target="_blank"
+                href={project.links[1].href}
+                className="project"
+                key={project.title}
+              >
+                <Image
+                  src={`/images/desktop/${project.iconName}.svg`}
+                  alt={`${project.iconName} icon`}
+                  width={50}
+                  height={50}
+                  priority
+                />
+                <p key={project.title}>{project.title}</p>
+              </Link>
+            </DragElement>
+          ))}
+        </section>
+        {vscodeIsMinimize && <VscodeBox />}
+      </C.container>
+    </>
   )
 }
 
--- src/features/desktop/pages/styles.ts
+++ src/features/desktop/pages/styles.ts
@@ -1,7 +1,4 @@
 import styled from 'styled-components'
-interface Props {
-  vscodeIsOpen: boolean
-}
 
 export const container = styled.main`
   height: 100%;
@@ -44,38 +41,3 @@ export const container = styled.main`
     z-index: 50;
   }
 `
-export const nav = styled.nav(
-  (props: Props) => `
-  width: 100%;
-  background-color: #1e1e1e;
-  border-top: 2px solid #2d2d2d;
-
-
-  a {
-    position: relative;
-    display: block;
-    padding: 10px;
-    width: fit-content;
-    background-color: ${props.vscodeIsOpen ? '#2d2d2d' : 'transparent'};
-    cursor: pointer;
-    transition: 0.5s;
-  }
-
-  a:hover {
-    background-color: #333333;
-  }
-
-  a::after {
-    content: '';
-    position: absolute;
-    display: ${props.vscodeIsOpen ? 'block' : 'none'};
-    bottom: -2px;
-    left: 50%;
-    transform: translate3d(-50%, -50%, 0);
-    width: 6px;
-    height: 6px;
-    border-radius: 50%;
-    background-color: #007acc;
-  }
-`
-)
