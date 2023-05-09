e8aa81e3c382e9ffaf6b7c23030eb28952d36584
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-09 00:13:35 -0300

    style: ensure responsive container element

--- src/layouts/vscode/styles.ts
+++ src/layouts/vscode/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const container = styled.main`
   background-color: #1e1e1e;
   position: absolute;
-  height: calc(100% - 23px);
+  height: calc(100% - 80px);
   width: 100%;
   display: flex;
   flex-direction: column;
