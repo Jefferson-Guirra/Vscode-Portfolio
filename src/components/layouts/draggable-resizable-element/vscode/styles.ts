c8cc69784e49a4b6ba577ea7e847b5ec3234892b
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-11 01:31:12 -0300

    style: add border in container element

--- src/components/layouts/draggable-resizable-element/vscode/styles.ts
+++ src/components/layouts/draggable-resizable-element/vscode/styles.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const container = styled.main`
   background-color: #1e1e1e;
+  border: 2px solid #2d2d2d;
+  border-radius: 3px;
   width: 100%;
   height: 100%;
   margin: auto;
