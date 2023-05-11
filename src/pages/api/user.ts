bcbf7bdce7650cf49d8b1732a25aa7650bd1cc50
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-11 18:16:01 -0300

    refactor: add environment variable for requests to github api

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -11,7 +11,15 @@ export default async function user(
   res: NextApiResponse<Data>
 ) {
   try {
-    const promise = await fetch('https://api.github.com/users/Jefferson-Guirra')
+    const promise = await fetch(
+      'https://api.github.com/users/Jefferson-Guirra',
+      {
+        headers: {
+          Authorization: process.env.GITHUB_PERSONAL_TOKEN as string,
+        },
+      }
+    )
+    console.log(promise)
     const { avatar_url, bios, name, url, public_repos } =
       (await promise.json()) as GithubApi
     const data: DataApiUser = {
